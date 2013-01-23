9b109ef6f2e8ee39dc99bc670f92b7150f2ed4f3
Rob <robert.carr@owasp.org>
2013-01-23 00:40:39 +0000

    Updated the licensing.
    Updated copyright notices to use the GNU General Public License v3 and
    updated the setup to use and distribute the new license.

--- yaircc/UI/TemporalTextBox.cs
+++ yaircc/UI/TemporalTextBox.cs
@@ -1,21 +1,20 @@
 ﻿//-----------------------------------------------------------------------
 // <copyright file="TemporalTextBox.cs" company="intninety">
-//     Copyright 2012 intninety
+//     An extended .NET TextBox control targetted at .NET 3.5 and above.
+//     Copyright (C) 2012-2013 Robert Carr
 //
-//     This file is part of TemporalTextBox.
-//
-//     TemporalTextBox is free software: you can redistribute it and/or modify
+//     This program is free software: you can redistribute it and/or modify
 //     it under the terms of the GNU General Public License as published by
 //     the Free Software Foundation, either version 3 of the License, or
 //     (at your option) any later version.
-//    
-//     TemporalTextBox is distributed in the hope that it will be useful,
+//
+//     This program is distributed in the hope that it will be useful,
 //     but WITHOUT ANY WARRANTY; without even the implied warranty of
 //     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //     GNU General Public License for more details.
 //
 //     You should have received a copy of the GNU General Public License
-//     along with TemporalTextBox.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
+//     along with this program.  If not, see http://www.gnu.org/licenses/.
 // </copyright>
 //-----------------------------------------------------------------------
 
